6729b9126081e32b81ad8e21d631d669a1d57a9e
akgandrew <a.greenhalgh@herts.ac.uk>
2022-12-16 13:54:04 +0000

    finding top 10 populations
    identified top 10 countries population wise in 2021 to compare, with dataframe containing population across years. Also allows countries to be exported as array to select in other dataframes later in code.

--- Statistics and Trends Report Assignment.py
+++ Statistics and Trends Report Assignment.py
@@ -5,6 +5,19 @@ Created on Mon Dec 12 18:33:59 2022
 @author: ag11afr
 """
 
+#import required modules
+
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+import pandas as pd
+
+
+# Make datafrane (df_all_data) for climate change large data set from xlsx file named appropriatly
+
+df_all_data = pd.read_excel("Climate_Change_Data_Set.xlsx")  
+
+
+
+print(df_all_data)
