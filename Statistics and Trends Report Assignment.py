39d7945e1e5e0e5d449d1591cfc8c74e055b7961
akgandrew <a.greenhalgh@herts.ac.uk>
2022-12-18 16:47:33 +0000

    Data tested for normaility
    All data columns tested for normality

--- Statistics and Trends Report Assignment.py
+++ Statistics and Trends Report Assignment.py
@@ -13,10 +13,11 @@ import matplotlib.pyplot as plt
 
 import pandas as pd
 
-from import_csv_to_DF_transposeimport import import_csv_to_DF_transpose
+import import_csv
 
+# using function import_csv_to_DF_transpose from module import_csv
 
-df_all_data, df_all_data_transposed = import_csv_to_DF_transpose("Climate_Change_Data_Set.xlsx")
+df_all_data, df_all_data_transposed = import_csv.import_csv_to_DF_transpose("Climate_Change_Data_Set.xlsx")
 
 # Make datafrane (df_all_data) for climate change large data set from xlsx file named appropriatly
 
@@ -69,8 +70,6 @@ top_10_country_names = df_pop_total_top_10['Country Name']
 # Extract the columns from the DataFrame to plot
 columns = df_pop_total_top_10.columns[5:]
 
-
-
 # Iterate through the rows of the DataFrame and plot each by country
 # divide values by 1,000,000,000 to simplify units for report
 
@@ -80,15 +79,72 @@ for i, row in df_pop_total_top_10.iterrows():
     population_data = population_data/1000000000
     plt.plot(columns, population_data, label=country_name)
 
-# Add a legend and show the plot
-plt.legend()
+# Add a legend and place it to the right of the graph
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
 
 # Label the y-axis
 plt.ylabel('Population (Billions)')
-
+plt.xlabel('Time (Years)')
 plt.show()
 
-
-
-
 print(df_all_data)
+
+# create function to analyse top 10 countries comparing two variables by factor
+# e.g. C02 vs forrest % etc
+
+# shifting to using transposed dataframe where 
+# countries and environmental factors arranged as columns
+
+# remove unwanted first 3 columns
+df_all_data_transposed = df_all_data_transposed.iloc[:, 3:]
+
+#make years and row subjects the index for row.
+df_all_data_transposed = df_all_data_transposed.set_index(3)
+
+# Convert top_10_country_names to a DataFrame
+df_top_10_country_names = top_10_country_names.to_frame()
+
+
+#identifies top 10 country data by columns
+columns_to_keep = df_all_data_transposed.columns[df_all_data_transposed.isin(top_10_country_names.values.flatten()).any()]
+
+##just keeps the data for the top 10 coutries population wise
+
+df_all_data_transposed = df_all_data_transposed.loc[:, columns_to_keep]
+
+# We are going to look at correlations so need to get rid of data with less 
+# than 50% (self selected cut off) of data for each variable
+
+# Create boolean df that indicates which values are NaN
+df_nan = df_all_data_transposed.isnull()
+
+# Count the number of NaN values in each column of data
+nan_count = df_nan.sum()
+
+# Select only the columns that have less than 25 NaN values 
+#25 chosen as first 4 rows will be nan (false) then 50% of 42 years 
+# is 21 so 21 + 4 =25
+df_filtered = df_all_data_transposed.loc[:, nan_count < 25]
+
+from scipy.stats import shapiro
+
+df = df_all_data_transposed
+# Loop through the columns of the dataframe
+for col, data in df.iteritems():
+    # Slice the data from row 4 to the last row
+    data_sliced = data.iloc[4:]
+
+    # Calculate the Shapiro-Wilk test for normality
+    stat, p = shapiro(data_sliced)
+
+    # If the p-value is less than 0.05, the data is not normal
+    if p < 0.05:
+        result = 'Data is not normal'
+    else:
+        result = 'Data is normal'
+
+    # Add a new row at the bottom of the column with the results of the Shapiro-Wilk test
+    df.at['Normality Result', col] = result
+    
+df_correlation_ready = df
+
